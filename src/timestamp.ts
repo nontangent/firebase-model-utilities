98a247f126791fdd8fd6fd6a924f0bf159be237a
nontangent <irregular0number@gmail.com>
2020-11-08 12:27:56 +0900

    Remove: timestamp

--- src/timestamp.ts
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import * as admin from 'firebase-admin';
-
-export type Timestamp = firebase.firestore.Timestamp | admin.firestore.Timestamp;
-export const Timestamp = firebase?.firestore?.Timestamp || admin?.firestore?.Timestamp;
-
-export const firestore = firebase.firestore || admin.firestore;
-// export type FieldValue = firebase.firestore.FieldValue | admin.firestore.FieldValue;
-export const FieldValue = firebase.firestore?.FieldValue || admin.firestore?.FieldValue;
